2f0ac1f18c3f80cb172b25c40e73f0d9a23895e6
Sayed Erfan Arefin <erfanjordison@gmail.com>
2018-09-17 23:00:29 +0600

    cdn added

--- gulp-tasks/phpdoc/replace.js
+++ gulp-tasks/phpdoc/replace.js
@@ -4,6 +4,6 @@ module.exports = function(gulp, callback) {
 	// replace string
 	return gulp.src([config.html + '/' + myTextDirection + '/' + myLayoutName + '/' + 'php' + '/' + '*.php'])
 		.pipe(replace('<!DOCTYPE html>', '<?php include "chkUserSession.php"; ?><!DOCTYPE html>'))
-		.pipe(replace('../../../', ''))
+		.pipe(replace('https://erfancdn.blob.core.windows.net/admin-template/', ''))
 		.pipe(gulp.dest(config.html + '/' + myTextDirection + '/' + myLayoutName + '/' + 'php' + '/'));
 };
